88cb8768f6ba5c6a7a86a1bcfd5a4ed265d4d3c3
Laguna101 <113820390+Laguna101@users.noreply.github.com>
2026-06-11 09:53:55 -0300

    Add additional app chunks to the chunks array

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const chunks = ["app-chunk-01.js","app-chunk-02.js","app-chunk-03.js","app-chunk-04.js","app-chunk-05.js","app-chunk-06.js"];
+  const chunks = ["app-chunk-01.js","app-chunk-02.js","app-chunk-03.js","app-chunk-04.js","app-chunk-05.js","app-chunk-06.js","app-chunk-07.js","app-chunk-08.js","app-chunk-09.js","app-chunk-10.js","app-chunk-11.js","app-chunk-12.js","app-chunk-13.js","app-chunk-14.js","app-chunk-15.js","app-chunk-16.js","app-chunk-17.js"];
   window.__AMPLA_APP_CODE = "";
   chunks.reduce((chain, src) => chain.then(() => new Promise((resolve, reject) => {
     const script = document.createElement("script");
